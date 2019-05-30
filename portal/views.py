70b68f7ba75e3e3307201b56d1f289065a6aeff2
Jaro <jeremyvan614@gmail.com>
2019-05-30 12:53:32 -0500

    user create and edit, group list implemented

--- portal/views.py
+++ portal/views.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     from urllib import urlencode
 
-from portal import app, database
+from portal import app
 from portal.decorators import authenticated
 from portal.utils import (load_portal_client, get_portal_tokens,
                           get_safe_redirect)
@@ -17,8 +17,8 @@ import sys
 sys.path.insert(0, '/etc/ciconnect/secrets')
 
 try:
-    f = open("/etc/slate/secrets/slate_api_token.txt", "r")
-    g = open("slate_api_endpoint.txt", "r")
+    f = open("/etc/ciconnect/secrets/ciconnect_api_token.txt", "r")
+    g = open("ciconnect_api_endpoint.txt", "r")
 except:
     # Use these two lines below on local
     f = open("/Users/JeremyVan/Documents/Programming/UChicago/CI_Connect/secrets/ciconnect_api_token.txt", "r")
@@ -37,15 +37,29 @@ def home():
 @app.route('/projects', methods=['GET'])
 def projects():
     """OSG Connect Projects"""
-    return render_template('projects.html')
+    if request.method == 'GET':
+        query = {'token': ciconnect_api_token}
+        projects = requests.get(ciconnect_api_endpoint + '/v1alpha1/groups', params=query)
+        projects = projects.json()['groups']
+        print(projects)
+        return render_template('projects.html', projects=projects)
 
 
 @app.route('/projects/new', methods=['GET', 'POST'])
-# @authenticated
+@authenticated
 def create_project():
     """Create Projects"""
     if request.method == 'GET':
         return render_template('projects_create.html')
+    elif request.method == 'POST':
+        return redirect(url_for('projects'))
+
+@app.route('/projects/some', methods=['GET', 'POST'])
+@authenticated
+def view_project():
+    """Detailed view of specific projects"""
+    if request.method == 'GET':
+        return render_template('project_profile.html')
 
 
 @app.route('/signup', methods=['GET'])
@@ -97,39 +111,134 @@ def logout():
     return redirect(''.join(ga_logout_url))
 
 
+@app.route('/profile/new', methods=['GET', 'POST'])
+@authenticated
+def create_profile():
+    identity_id = session.get('primary_identity')
+    institution = session.get('institution')
+    globus_id = identity_id
+    query = {'token': ciconnect_api_token,
+             'globus_id': globus_id}
+
+    if request.method == 'GET':
+        return render_template('profile_create.html')
+
+    elif request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone-number']
+        institution = request.form['institution']
+        globus_id = session['primary_identity']
+
+        superuser = False
+        service_account = False
+        # Schema and query for adding users to CI Connect DB
+        post_user = {"apiVersion": 'v1alpha1',
+                    'metadata': {'globusID': globus_id, 'name': name, 'email': email,
+                                 'phone': phone, 'institution': institution,
+                                 'public_key': public_key, 'superuser': superuser,
+                                 'service_account': service_account}}
+
+        r = requests.post(ciconnect_api_endpoint + '/v1alpha1/users', params=query, json=post_user)
+        print("Created User: ", r)
+
+        if 'next' in session:
+            redirect_to = session['next']
+            session.pop('next')
+        else:
+            redirect_to = url_for('profile')
+
+        return redirect(url_for('profile'))
+
+
+@app.route('/profile/edit/<user_id>', methods=['GET', 'POST'])
+@authenticated
+def edit_profile(user_id):
+    identity_id = session.get('primary_identity')
+    access_token = session.get('access_token')
+    globus_id = identity_id
+    query = {'token': access_token,
+             'globus_id': globus_id}
+    if request.method == 'GET':
+        # Get user info, pass through as args, convert to json and load input fields
+        profile = requests.get(
+                    ciconnect_api_endpoint + '/v1alpha1/users/' + user_id, params=query)
+        profile = profile.json()['metadata']
+
+        return render_template('profile_edit.html', profile=profile)
+
+    elif request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone-number']
+        institution = request.form['institution']
+        public_key = request.form['sshpubstring']
+
+        globus_id = session['primary_identity']
+        superuser = True
+        service_account = False
+        # Schema and query for adding users to CI Connect DB
+        post_user = {"apiVersion": 'v1alpha1',
+                    'metadata': {'name': name, 'email': email,
+                                 'phone': phone, 'institution': institution,
+                                 'public_key': public_key}}
+        r = requests.put(ciconnect_api_endpoint + '/v1alpha1/users/' + user_id, params=query, json=post_user)
+        print("Updated User: ", r)
+
+        if 'next' in session:
+            redirect_to = session['next']
+            session.pop('next')
+        else:
+            redirect_to = url_for('profile')
+
+        return redirect(url_for('profile'))
+
+
 @app.route('/profile', methods=['GET', 'POST'])
 @authenticated
 def profile():
     """User profile information. Assocated with a Globus Auth identity."""
     if request.method == 'GET':
         identity_id = session.get('primary_identity')
-        profile = database.load_profile(identity_id)
+        profile = None
+        query = {'token': ciconnect_api_token,
+                 'globus_id': identity_id}
+
+        user = requests.get(ciconnect_api_endpoint + '/v1alpha1/find_user', params=query)
+        user = user.json()
+        user_id = user['metadata']['id']
+        user_token = user['metadata']['access_token']
+
+        profile = requests.get(
+                    ciconnect_api_endpoint + '/v1alpha1/users/' + user_id, params=query)
+        profile = profile.json()
 
         if profile:
-            name, email, institution = profile
-
-            session['name'] = name
-            session['email'] = email
-            session['institution'] = institution
+            profile = profile['metadata']
+            name = profile['name']
+            email = profile['email']
+            phone = profile['phone']
+            institution = profile['institution']
+            ssh_pubkey = profile['public_key']
         else:
             flash(
                 'Please complete any missing profile fields and press Save.', 'warning')
 
         if request.args.get('next'):
             session['next'] = get_safe_redirect()
+        return render_template('profile.html', profile=profile)
 
-        return render_template('profile.html')
     elif request.method == 'POST':
         name = session['name'] = request.form['name']
         email = session['email'] = request.form['email']
         institution = session['institution'] = request.form['institution']
+        globus_id = session['primary_identity']
+        phone = request.form['phone-number']
+        public_key = request.form['sshpubstring']
+        superuser = True
+        service_account = False
 
-        database.save_profile(identity_id=session['primary_identity'],
-                              name=name,
-                              email=email,
-                              institution=institution)
-
-        flash('Thank you! Your profile has been successfully updated.', 'success')
+        # flash('Thank you! Your profile has been successfully updated.', 'success')
 
         if 'next' in session:
             redirect_to = session['next']
@@ -178,21 +287,37 @@ def authcallback():
             is_authenticated=True,
             name=id_token.get('name', ''),
             email=id_token.get('email', ''),
-            institution=id_token.get('institution', ''),
+            institution=id_token.get('organization', ''),
             primary_username=id_token.get('preferred_username'),
             primary_identity=id_token.get('sub'),
         )
 
-        profile = database.load_profile(session['primary_identity'])
+        # print("URL ROOT CAME FROM: {}".format(request.url_root))
+        globus_id = session['primary_identity']
+        query = {'token': ciconnect_api_token,
+                 'globus_id': globus_id}
+
+        r = requests.get(
+            ciconnect_api_endpoint + '/v1alpha1/find_user', params=query)
+        # print("AUTH: {}".format(r.json()))
+        user_info = r.json()
+        user_access_token = user_info['metadata']['access_token']
+        user_id = user_info['metadata']['id']
+        profile = requests.get(
+                    ciconnect_api_endpoint + '/v1alpha1/users/' + user_id, params=query)
+        profile = profile.json()
+        # print("PROFILE: {}".format(profile))
 
         if profile:
-            name, email, institution = profile
+            profile = profile['metadata']
 
-            session['name'] = name
-            session['email'] = email
-            session['institution'] = institution
+            session['name'] = profile['name']
+            session['email'] = profile['email']
+            session['institution'] = profile['institution']
+            session['access_token'] = profile['access_token']
+            session['url_root'] = request.url_root
         else:
-            return redirect(url_for('profile',
+            return redirect(url_for('create_profile',
                             next=url_for('profile')))
 
         return redirect(url_for('profile'))
