9e1bf5edbad59b926b937f473e2ed89ca5983e1f
Jaro <jeremyvan614@gmail.com>
2019-06-03 11:09:14 -0500

    404 and error route handling, display unix user name

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,6 @@
 from flask import (abort, flash, redirect, render_template, request,
                    session, url_for)
-import requests
+import requests, traceback, json
 
 try:
     from urllib.parse import urlencode
@@ -26,6 +26,27 @@ except:
 
 ciconnect_api_token = f.read().split()[0]
 ciconnect_api_endpoint = g.read().split()[0]
+
+# Create a custom error handler for Exceptions
+@app.errorhandler(Exception)
+def exception_occurred(e):
+    trace = traceback.format_tb(sys.exc_info()[2])
+    app.logger.error("{0} Traceback occurred:\n".format(time.ctime()) +
+                     "{0}\nTraceback completed".format("n".join(trace)))
+    trace = "<br>".join(trace)
+    trace.replace('\n', '<br>')
+    return render_template('error.html', exception=trace,
+                           debug=app.config['DEBUG'])
+
+@app.route('/error', methods=['GET'])
+def errorpage():
+    if request.method == 'GET':
+        return render_template('error.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("404.html")
 
 
 @app.route('/', methods=['GET'])
