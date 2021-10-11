845de34533ca130a7bbe040253042c198e9e0c66
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-10-11 17:08:25 -0500

    add TOTP secret string from API and option to create TOTP secrets in the API

--- portal/views.py
+++ portal/views.py
@@ -1767,6 +1767,7 @@ def create_profile():
         globus_id = session['primary_identity']
         superuser = False
         service_account = False
+        create_totp_secret = False 
 
         post_user = {"apiVersion": 'v1alpha1',
                      'metadata': {'globusID': globus_id, 'name': name,
@@ -1774,7 +1775,9 @@ def create_profile():
                                   'institution': institution,
                                   'unix_name': unix_name,
                                   'superuser': superuser,
-                                  'service_account': service_account}}
+                                  'service_account': service_account,
+                                  'create_totp_secret': create_totp_secret,
+                    }}
 
         # print("POSTED: {}".format(post_user))
         r = requests.post(ciconnect_api_endpoint +
@@ -1874,6 +1877,10 @@ def edit_profile(unix_name):
         email = request.form['email']
         phone = request.form['phone-number']
         institution = request.form['institution']
+        if request.form.get("totpsecret") is not None:
+            create_totp_secret = True
+        else: 
+            create_totp_secret = False
         try:
             public_key = request.form['sshpubstring']
         except:
@@ -1892,12 +1899,14 @@ def edit_profile(unix_name):
                          'metadata': {'name': name, 'email': email,
                                       'phone': phone, 'institution': institution,
                                       'public_key': public_key,
+                                      'create_totp_secret': create_totp_secret,
                                       'additional_attributes': additional_metadata}}
         else:
             post_user = {"apiVersion": 'v1alpha1',
                          'metadata': {'name': name, 'email': email,
                                       'phone': phone, 'institution': institution,
                                       'public_key': '',
+                                      'create_totp_secret': create_totp_secret,
                                       'additional_attributes': additional_metadata}}
         # PUT request to update user information
         r = requests.put(ciconnect_api_endpoint + '/v1alpha1/users/' +
@@ -1941,6 +1950,21 @@ def profile():
         # print(profile)
         if profile:
             profile = profile['metadata']
+            # The auth string should never get used if the totp_secret key doesn't exist anyhow.
+            try:
+                issuer = quote(session["url_host"]["display_name"])
+                authenticator_string = (
+                    "otpauth://totp/"
+                    + unix_name
+                    + "?secret="
+                    + profile["totp_secret"]
+                    + "&issuer="
+                    + issuer
+                )
+            except KeyError as e:
+                print("Couldn't find a totp_secret in the profile for ", unix_name)
+                authenticator_string = None
+           
             # Check User's Status in OSG Group specifcally
             user_status = requests.get(ciconnect_api_endpoint
                                        + '/v1alpha1/users/'
@@ -1974,7 +1998,8 @@ def profile():
             session['next'] = get_safe_redirect()
         return render_template('profile.html', profile=profile,
                                user_status=user_status,
-                               user_login_nodes=user_login_nodes)
+                               user_login_nodes=user_login_nodes,
+                               authenticator_string=authenticator_string)
 
     elif request.method == 'POST':
         if 'next' in session:
